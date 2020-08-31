6139e2c0fbc04da847e9bbb9de3c1bfd867e31e7
Santosh <santosh.sw@planetc.net>
2020-08-31 23:51:42 +0530

    Updates

---
+++ precache-manifest.f4c58f2a82b450e5a88e53cfc3cac43d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0059a4e13fabff9522f41c2a0f433c4d",
+    "url": "/diagnal_task/index.html"
+  },
+  {
+    "revision": "2b3d362ab005ce32c82c",
+    "url": "/diagnal_task/static/css/main.00913883.chunk.css"
+  },
+  {
+    "revision": "975dc9290fff737a2e39",
+    "url": "/diagnal_task/static/js/2.d357f39f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/diagnal_task/static/js/2.d357f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3d362ab005ce32c82c",
+    "url": "/diagnal_task/static/js/main.fbe6b819.chunk.js"
+  },
+  {
+    "revision": "39b9116e134762c5386d",
+    "url": "/diagnal_task/static/js/runtime-main.ffaf236b.js"
+  }
+]);
